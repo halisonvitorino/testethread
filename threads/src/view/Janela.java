58a8887eeebfae0f6cc46daac87ada2e9a58bd4b
halison.vitorino <halison.vitorino@halisonribeiro.foton.local>
2016-04-29 14:20:15 -0300

    8

--- threads/src/view/Janela.java
+++ threads/src/view/Janela.java
@@ -1,6 +1,7 @@
 package view;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
@@ -8,65 +9,61 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SpringLayout;
 import control.CarroThread;
 
-
 public class Janela extends JFrame implements ActionListener {
-
-private JPanel jpPrincipal, jpSecundario;
-private JButton btnCorrida, btnCancel;
-private JLabel lblPista ;
-private ImageIcon imgCarro1, imgCarro2, imgCarro3, pista;
+	private JFrame jfJanela;
+	private JPanel jpPrincipal, jpSecundario;
+	private JButton btnCorrida, btnCancel;
+	private ImageIcon imgCarro1, imgCarro2, imgCarro3, pista;
 
 public Janela() {
+	jfJanela = new JFrame();
+	jfJanela.setSize(1000,600);
+	jfJanela.setLocationRelativeTo(null);
+	jfJanela.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10)); 
+	jfJanela.setResizable(false);
+
+	jpPrincipal = new JPanel();
+	jpPrincipal.setPreferredSize(new Dimension(900, 550)); 
+	jpPrincipal.setLayout(null); 
+	jpPrincipal.setBackground(Color.black);
+	
+	//jpSecundario = new JPanel();
+	//jpSecundario.setPreferredSize(new Dimension(200,200));
+	//jpSecundario.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 500)); 
+	//jpSecundario.setBackground(Color.BLUE);
+	
+	ImageIcon img = new ImageIcon("./src/imagens/pista.png");
+	JLabel label  = new JLabel(img);
+	label.setBounds(300, 100, 300, 100);
+		
+	imgCarro1   = new ImageIcon("./src/imagens/carro1.png");
+	imgCarro2   = new ImageIcon("./src/imagens/carro2.png");
+	imgCarro3   = new ImageIcon("./src/imagens/carro3.png");
 	
 	
-setSize(500,500);
-SpringLayout layout = new SpringLayout();
-setBackground(Color.blue);
-setVisible(true);
-
-jpPrincipal = new JPanel(layout);
-jpPrincipal.setPreferredSize(new Dimension(100,100));
-jpPrincipal.setBackground(Color.BLACK);
-
-
-jpSecundario = new JPanel();
-jpSecundario.setPreferredSize(new Dimension(200,200));
-jpSecundario.setBackground(Color.BLUE);
-
-
-pista       = new ImageIcon("./src/imagens/pista.png");
-imgCarro1   = new ImageIcon("./src/imagens/carro1.png");
-imgCarro2   = new ImageIcon("./src/imagens/carro2.png");
-imgCarro3   = new ImageIcon("./src/imagens/carro3.png");
-
-//CRIANDO BOTOES
-btnCorrida = new JButton("Start");
-btnCancel  = new JButton("Stop");
-btnCorrida.setBounds(500, 550, 120, 50);
-btnCancel .setBounds(640, 550, 120, 50);
-btnCorrida.addActionListener(this);
-btnCancel .addActionListener(this);
-
-//CRIANDO BOTOES
-btnCorrida = new JButton("Start");
-btnCancel  = new JButton("Stop");
-btnCorrida.setBounds(500, 550, 120, 50);
-btnCancel .setBounds(640, 550, 120, 50);
-
-add(jpPrincipal);
-
-jpPrincipal.add(btnCancel);
-jpPrincipal.add(btnCorrida);
-
-jpSecundario.setVisible(true);
-jpPrincipal.setVisible(true);
-
-this.repaint();
-
-
+	//CRIANDO BOTOES
+	btnCorrida = new JButton("Start");
+	btnCancel  = new JButton("Stop");
+	btnCorrida.setBounds(300, 450, 100, 50);
+	btnCancel .setBounds(500, 450, 100, 50);
+	btnCorrida.addActionListener(this);
+	btnCancel .addActionListener(this);
+	
+	jfJanela.add(jpPrincipal);
+	//jfJanela.add(jpSecundario);
+	
+	jpPrincipal.add(label);
+		
+	jpPrincipal.add(btnCancel);
+	jpPrincipal.add(btnCorrida);
+	
+	//jpSecundario.setVisible(true);
+	jfJanela.setVisible(true);
+	jpPrincipal.setVisible(true);
+		
+	this.repaint();
 }
 
 
@@ -83,10 +80,13 @@ return carro;
 // PROGRAMANDO A AÇÃO DOS BOTÕES
 @Override
 public void actionPerformed(ActionEvent dispara) {if (dispara.getSource() == this.btnCorrida) {
-	this.lblPista.add(JLabelCarros("Carro1", imgCarro1, 0, 50));
-	this.lblPista.add(JLabelCarros("Carro1", imgCarro2, 0, 200));
-	this.lblPista.add(JLabelCarros("Carro1", imgCarro3, 0, 350));
+	this.jpPrincipal.add(JLabelCarros("Carro1", imgCarro1, 0, 100));
+	this.jpPrincipal.add(JLabelCarros("Carro1", imgCarro2, 0, 100));
+	this.jpPrincipal.add(JLabelCarros("Carro1", imgCarro3, 0, 100));
 	}
+
+
+
 
 if (dispara.getSource() == this.btnCancel) {
 	System.exit(0);
